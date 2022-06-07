f63834481f64428d0d936fbc2704bdab74170805
Crisesm <cristianesem2@gmail.com>
2022-06-06 22:55:55 -0300

    fix cors

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ require('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productsRouter = require('./routes/products');
-
 var app = express();
+const cors = require("cors"); 
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
