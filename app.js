9acdb848f9d937db4558710e9aa0c63d2c456cbd
Crisesm <cristianesem2@gmail.com>
2022-06-11 12:53:15 -0300

    productos sin token

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
-app.use('/api/products', jwtConfig.verifyToken, productsRouter);
+app.use('/api/products', /* jwtConfig.verifyToken, */ productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
